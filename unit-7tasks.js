ad2813fda4b50a8e73827551be898fe07bbee32f
HacQueen <80148093+SMaftuna-creator@users.noreply.github.com>
2022-05-24 16:51:29 +0500

    Update unit-7tasks.js

--- unit-7tasks.js
+++ unit-7tasks.js
@@ -3,4 +3,4 @@ for (const match of 'ahb acb aeb aeeb adcb axeb'.matchAll(/a.b/g)) {
 }
 
 let date = new Date();
-console.log( date.getHours(), date.getMinutes(), date.getSeconds(), date.getDate(), date.getMonth(), date.getFullYear() );
+console.log( date.getFullYear(), date.getDate(), date.getMonth() );
